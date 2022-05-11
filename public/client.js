7253dc55e9a32765b1bd7a2986415941199b0ef5
Nathi Geigant <ngeigant@gmail.com>
2022-05-11 09:20:42 +0200

    Added sum of dice throws and added some overall styling

--- public/client.js
+++ public/client.js
@@ -1,53 +1,60 @@
 const socket = io();
 
-const formUser = document.querySelector("#formUser");
-const inputUser = document.querySelector("#inputUser");
-const messages = document.querySelector("#messages");
-const formMessage = document.querySelector("#formMessage");
-const inputMessage = document.querySelector("#inputMessage");
-const userContianer = document.querySelector("#userContainer");
+const formUser = document.querySelector('#formUser');
+const inputUser = document.querySelector('#inputUser');
+const message = document.querySelector('.message');
+const formMessage = document.querySelector('#formMessage');
+const inputMessage = document.querySelector('#inputMessage');
+const userContainer = document.querySelector('#userContainer');
 
-const throwDice = document.querySelector("#throwDice");
-const diceContainer = document.querySelector("#diceContainer");
-const diceThrows = document.querySelector("#diceThrows");
+const throwDice = document.querySelector('#throwDice');
+const diceThrows = document.querySelector('#diceThrows');
 
 let myUser;
 
-formUser.addEventListener("submit", function (e) {
-  e.preventDefault();
-  myUser = inputUser.value;
-  userContianer.innerHTML = "<h2>Välkommen " + myUser + "</h2>";
-  document.getElementById("user").style.display = "none";
-  document.getElementById("message").style.display = "block";
+formUser.addEventListener('submit', function (e) {
+    e.preventDefault();
+    myUser = inputUser.value;
+    userContainer.innerHTML = '<h2>Welcome ' + myUser + '</h2>';
+    document.getElementById('user').style.display = 'none';
+    document.getElementById('message').style.display = 'block';
 });
 
-formMessage.addEventListener("submit", function (e) {
-  e.preventDefault();
-  if (inputMessage.value) {
-    socket.emit("chatMessage", { user: myUser, message: inputMessage.value });
-    inputMessage.value = "";
-  }
+formMessage.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (inputMessage.value) {
+        socket.emit('chatMessage', {
+            user: myUser,
+            message: inputMessage.value,
+        });
+        inputMessage.value = '';
+    }
 });
 
-socket.on("newChatMessage", function (msg) {
-  let item = document.createElement("li");
-  item.textContent = msg;
-  messages.appendChild(item);
+socket.on('newChatMessage', function (msg) {
+    let item = document.createElement('li');
+    item.textContent = msg;
+    message.appendChild(item);
 });
 
-throwDice.addEventListener("click", function (e) {
-  e.preventDefault();
-  let myDiceThrow = Math.floor(Math.random() * 6 + 1);
-  if (myDiceThrow) {
-    socket.emit("rollTheDice", { user: myUser, diceRoll: myDiceThrow });
-  }
-  console.log(myDiceThrow);
+let sumOfNumber = [];
+throwDice.addEventListener('click', function (e) {
+    let myDiceThrow = Math.floor(Math.random() * 6 + 1);
+    sumOfNumber.push(myDiceThrow);
+    let sum = sumOfNumber.reduce((a, b) => {
+        return a + b;
+    });
+    socket.emit('rollTheDice', {
+        user: myUser,
+        diceRoll: myDiceThrow,
+        sumOfRolls: sum,
+    });
+    console.log(myDiceThrow, sum);
 });
 
-socket.on("newDiceRoll", function (roll) {
-  console.log(roll);
-  let item = document.createElement("li");
-  item.innerHTML = roll;
-  diceThrows.appendChild(item);
-  console.log(item);
+socket.on('newDiceRoll', function (roll) {
+    console.log(roll);
+    let item = document.createElement('li');
+    item.innerHTML = roll;
+    diceThrows.appendChild(item);
 });
--- server.js
+++ server.js
@@ -1,30 +1,34 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
+const http = require('http');
 const server = http.createServer(app);
-const io = require("socket.io")(server);
+const io = require('socket.io')(server);
 const port = 3000;
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-io.on("connection", (socket) => {
-  console.log(`A client with id ${socket.id} connected to the chat!`);
+io.on('connection', (socket) => {
+    console.log(`A client with id ${socket.id} connected to the chat!`);
 
-  socket.on("chatMessage", (msg) => {
-    console.log("Meddelande: " + msg.user + " " + msg.message);
-    io.emit("newChatMessage", msg.user + " : " + msg.message);
-  });
+    socket.on('chatMessage', (msg) => {
+        console.log('Meddelande: ' + msg.user + ' ' + msg.message);
+        io.emit('newChatMessage', msg.user + ' : ' + msg.message);
+    });
 
-  socket.on("rollTheDice", (value) => {
-    console.log("Meddelande: " + value.user + " " + value.diceRoll);
-    io.emit("newDiceRoll", value.user + " : " + value.diceRoll);
-  });
+    socket.on('rollTheDice', (value) => {
+        console.log('Meddelande: ' + value.user + ' ' + value.diceRoll);
+        // io.emit("newDiceRoll", value.user + " : " + value.diceRoll);
+        io.emit(
+            'newDiceRoll',
+            `${value.user} rolled ${value.diceRoll}! Sum of ${value.user}s rolls: ${value.sumOfRolls}`
+        );
+    });
 
-  socket.on("disconnect", () => {
-    console.log(`Client ${socket.id} disconnected!`);
-  });
+    socket.on('disconnect', () => {
+        console.log(`Client ${socket.id} disconnected!`);
+    });
 });
 
 server.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
